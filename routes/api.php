5194b6e360c6f6d9c69af4abd79fe5f4d79d4a86
Bettina30 <bettinathomas30@gmail.com>
2024-05-13 15:26:53 +0530

    up

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EmployeeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+/* Employee */
+Route::get('/employee', [EmployeeController::class, 'index']);
+Route::post('/employee', [EmployeeController::class, 'store']);
+Route::get('/employee/{employee}', [EmployeeController::class, 'show']);
+Route::put('/employee/{employee}', [EmployeeController::class, 'update']);
+Route::delete('/employee/{employee}', [EmployeeController::class, 'destroy']);
