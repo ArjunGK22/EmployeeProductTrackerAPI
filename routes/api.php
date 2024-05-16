2f7ca113556b0062e40ba4745594518eea8adea7
Arjun <arjungk22@outlook.com>
2024-05-16 10:20:39 +0530

    Authentication feature added

--- routes/api.php
+++ routes/api.php
@@ -4,48 +4,45 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ProductIssueReturnController;
 use App\Http\Controllers\UserController;
-use App\Models\Product;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EmployeeController;
 use App\Http\Controllers\PDFGenerationController;
 use App\Http\Controllers\TransactionController;
-use App\Models\Transaction;
-use Codedge\Fpdf\Fpdf\Fpdf;
+
 use App\Http\Controllers\EmployeeExportController;
 
 Route::get('/auth/login', [UserController::class, 'index']);
-Route::post('/auth/login', [AuthController::class, 'login']);
+Route::post('/auth/login', [AuthController::class, 'login']); //User Login
+Route::post('/auth/logout', [AuthController::class, 'logout']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
+    
+    //Only Admin Access
+    Route::group(['middleware' => ['role:admin']], function () {
+        
+        //Generate PDF based on Transactions
+        Route::get('/', [PDFGenerationController::class, 'generatePDF']);
+        
+        //Product Management Endpoints (Admin)
+        Route::resource('products', ProductController::class);
 
-    
+        //Transaction (Product Issue/Return) Endpoints (Admin)
+        Route::post('/transactions/issue', [ProductIssueReturnController::class, 'issueProducts']); //store new product
+        Route::post('/transactions/return', [ProductIssueReturnController::class, 'returnProducts']); //return product
 
-    Route::get('/',[PDFGenerationController::class,'generatePDF']);
-    
-    
-    
-    //Product Management Endpoints (Admin)
-    
-    Route::resource('products', ProductController::class);
-    
-    Route::post('/transactions/issue', [ProductIssueReturnController::class, 'issueProducts']); //store new product
-    Route::post('/transactions/return', [ProductIssueReturnController::class, 'returnProducts']); //return product
-    
-    Route::get('/transactions', [TransactionController::class, 'index']); //show all transactions 
-    
-    
-    
-    /* Employee */
-    Route::get('/employee', [EmployeeController::class, 'index']); 
-    Route::post('/employee', [EmployeeController::class, 'store']);
-    Route::get('/employee/{employee}', [EmployeeController::class, 'show']);
-    Route::put('/employee/{employee}', [EmployeeController::class, 'update']);
-    Route::delete('/employee/{employee}', [EmployeeController::class, 'destroy']);
+        Route::get('/transactions', [TransactionController::class, 'index']); //show all transactions 
+
+
+        /* Employee */
+        Route::get('/employee', [EmployeeController::class, 'index']);
+        Route::post('/employee', [EmployeeController::class, 'store']);
+        Route::get('/employee/{employee}', [EmployeeController::class, 'show']);
+        Route::put('/employee/{employee}', [EmployeeController::class, 'update']);
+        Route::delete('/employee/{employee}', [EmployeeController::class, 'destroy']);
+    });
+
+
 });
-
-// app/Http/routes.php | app/routes/web.php
-
 
 //excel
 Route::get('/employees/export', [EmployeeExportController::class, 'export']);
